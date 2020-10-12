1a8a75174d47357e76466ae1b2c3ecc97eed5f02
DailyRBLX <60293305+DailyRBLX@users.noreply.github.com>
2020-10-12 19:43:03 +0200

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,42 @@
+local Places = {
+	--[] = {Script = "",Name = ""},
+	[1962086868] = {Script = "https://raw.githubusercontent.com/DailyRBLX/sine-hub/main/Tower%20of%20Hell.lua",Name = "Tower of Hell"},
+}
+
+function initplace()
+	if Places[game.PlaceId] then
+		local info = Places[game.PlaceId]
+		local script = info.Script
+		local gamename = info.Name
+		local scrngui = Instance.new("ScreenGui")
+		local textlabel = Instance.new("TextLabel")
+		textlabel.BackgroundTransparency = 1
+		textlabel.TextScaled = true;
+		textlabel.Font = Enum.Font.SourceSansSemiBold
+		textlabel.TextColor3 = Color3.new(1,1,1)
+		textlabel.TextTransparency = 1;
+		textlabel.Text = string.format("%s %s","Game:",gamename)
+		textlabel.AnchorPoint.X = 0.5
+		textlabel.AnchorPonit.Y = 0.5
+		textlabel.Position = UDim2.new(0.5,0,0.5,0)
+		textlabel.Size = UDim2.new(0.45,0,0.09,0)
+
+		textlabel.Parent = scrngui
+		scrngui.Parent = game:GetService("Players").LocalPlayer.PlayerGui
+		for i = 0,1,0.1 do
+			textlabel.TextTransparency = i
+			wait(0.01)
+		end
+
+		wait(10)
+		for i = 1,0,0.1 do
+			textlabel.TextTransparency = i
+			wait(0.05)
+		end
+
+		loadstring(game:HttpGet(script))()
+		scrngui:Destroy()
+	end
+end
+
+initplace()
