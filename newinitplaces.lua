fbf0f4fa7c8ac682b238b249b9b79f2ef751153a
DailyRBLX <60293305+DailyRBLX@users.noreply.github.com>
2020-12-06 17:40:04 +0100

    Update newinitplaces.lua

--- newinitplaces.lua
+++ newinitplaces.lua
@@ -4,4 +4,5 @@ return {
 	[3582763398] = {Script = "https://raw.githubusercontent.com/DailyRBLX/sine-hub/main/Tower%20of%20Hell.lua",Name = "Tower of Hell"},
 	[5253186791] = {Script = "https://raw.githubusercontent.com/DailyRBLX/sine-hub/main/Tower%20of%20Hell.lua",Name = "Tower of Hell"},
 	[2603725502] = {Script = "https://raw.githubusercontent.com/DailyRBLX/sine-hub/main/a%20bizarre%20day.lua",Name = "A Bizarre Day Modded"},
+	[5278850819] = {Script = "https://raw.githubusercontent.com/DailyRBLX/sine-hub/main/stayaliveandflexyourtimeonothers.lua",Name = "lmao yeet"},
 }
