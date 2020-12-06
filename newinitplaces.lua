a6f76905abbfddf9f53bdf54d4bbf800926543f9
DailyRBLX <60293305+DailyRBLX@users.noreply.github.com>
2020-12-06 17:37:14 +0100

    Add files via upload

---
+++ newinitplaces.lua
@@ -0,0 +1,7 @@
+return {
+	--[] = {Script = "",Name = ""},
+	[1962086868] = {Script = "https://raw.githubusercontent.com/DailyRBLX/sine-hub/main/Tower%20of%20Hell.lua",Name = "Tower of Hell"},
+	[3582763398] = {Script = "https://raw.githubusercontent.com/DailyRBLX/sine-hub/main/Tower%20of%20Hell.lua",Name = "Tower of Hell"},
+	[5253186791] = {Script = "https://raw.githubusercontent.com/DailyRBLX/sine-hub/main/Tower%20of%20Hell.lua",Name = "Tower of Hell"},
+	[2603725502] = {Script = "https://raw.githubusercontent.com/DailyRBLX/sine-hub/main/a%20bizarre%20day.lua",Name = "A Bizarre Day Modded"},
+}
