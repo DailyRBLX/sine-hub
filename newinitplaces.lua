8a4923c4f74fdf8a8a550727ed6f1895a1200fcc
DailyRBLX <60293305+DailyRBLX@users.noreply.github.com>
2021-02-19 14:54:00 +0100

    Delete newinitplaces.lua

--- newinitplaces.lua
+++
@@ -1,8 +0,0 @@
-return {
-	--[] = {Script = "",Name = ""},
-	[1962086868] = {Script = "https://raw.githubusercontent.com/DailyRBLX/sine-hub/main/Tower%20of%20Hell.lua",Name = "Tower of Hell"},
-	[3582763398] = {Script = "https://raw.githubusercontent.com/DailyRBLX/sine-hub/main/Tower%20of%20Hell.lua",Name = "Tower of Hell"},
-	[5253186791] = {Script = "https://raw.githubusercontent.com/DailyRBLX/sine-hub/main/Tower%20of%20Hell.lua",Name = "Tower of Hell"},
-	[2603725502] = {Script = "https://raw.githubusercontent.com/DailyRBLX/sine-hub/main/a%20bizarre%20day.lua",Name = "A Bizarre Day Modded"},
-	[5278850819] = {Script = "https://raw.githubusercontent.com/DailyRBLX/sine-hub/main/stayaliveandflexyourtimeonothers.lua",Name = "lmao yeet"},
-}
